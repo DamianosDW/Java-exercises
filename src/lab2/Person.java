12e2778ef53300c7fd562a63b77f09cf29baa822
DamianosDW <damianosdw@gmail.com>
2018-10-13 16:41:37 +0200

    Add constructor and variables to Person.java class

--- src/lab2/Person.java
+++ src/lab2/Person.java
@@ -2,5 +2,11 @@ package lab2;
 
 public class Person
 {
-
+    public String name;
+    public String surname;
+    public Person(String name, String surname)
+    {
+        this.name = name;
+        this.surname = surname;
+    }
 }
