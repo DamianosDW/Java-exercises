eeb5fff42f2d7ec179afddf23654813ffaa8776d
DamianosDW <damianosdw@gmail.com>
2018-11-04 15:12:52 +0100

    Create loadAppWindow() method and getter - app version 0.8

--- src/lab4/SeaBattleGame.java
+++ src/lab4/SeaBattleGame.java
@@ -11,27 +11,39 @@ public class SeaBattleGame extends Application
 {
     private static final String fxmlPath = "main.fxml";
 
+    private static Stage stage;
+
     public static void main(String[] args) {
         // Start app
         launch(args);
     }
     @Override
     public void start(Stage primaryStage) {
+        stage = primaryStage;
         primaryStage.setTitle("SeaBattle");
 
-        // Create FXMLLoader
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlPath));
+        loadAppWindow();
 
-        try {
-            // Create scene and load main app window to stage
-            Scene scene = new Scene(fxmlLoader.load());
-            primaryStage.setScene(scene);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         // Lock app window size
         primaryStage.setResizable(false);
         // Show app window
         primaryStage.show();
+    }
+    // Getter
+    public static Stage getStage() {
+        return stage;
+    }
+    // Load app window
+    public static void loadAppWindow()
+    {
+        // Create FXMLLoader
+        FXMLLoader fxmlLoader = new FXMLLoader(SeaBattleGame.class.getResource(fxmlPath));
+        try {
+            // Create scene and load main app window to stage
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
